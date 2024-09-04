216c79dd47262081bff916fa8083b3671e0fac48
BaLLducked <westth2003@gmail.com>
2024-09-04 12:57:08 -0400

    new functions

--- buildamon.py
+++ buildamon.py
@@ -7,7 +7,14 @@ type_count = (0,1,2)
 mon_types = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting", "Poison",
     "Ground", "Flying", "Phychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
 starter_main_types = ["Fire", "Water", "Grass", "Electric"]
-mon_gimmick = ["None", "Mega Evolution", "Additional Form", "Signiture Move", "Signiture Ability", "Gigantamax", "New Mechanic", "Corrupted", "Shinning"]
+mon_gimmick = ["None", "Mega Evolution", "Additional Form", "Signiture Move", "Signiture Ability", "Fusion", "Gigantamax", "New Mechanic", "Corrupted", "Shinning"]
+gimmick_method = ["Held Item", "Seasonal", "Key Item", "Move", "Location", "Time", "Weather", "Ability"]
+aquire_method = ["Route Encounter", "Seasonal", "Ruins", "Hidden Grotto", "Tree", "Cave", "Beach", "Fishing", "Swimming", "Deep Water Swimming",
+                "Random World Encounter", "NPC Trade", "Special"]
+special_aquire_method = ["Story", "Scripted Event", "Post Story Ecounter", "Side Quest Encoutner", "Limited Time Event", "Gift"]
+mon_has_cosmetic_forms = False
+
+debug = False
 
 def rand_mon_strength():
     global strength
@@ -15,8 +22,6 @@ def rand_mon_strength():
     if debug == True:
         print(f"\n{strength}")
     return strength
-
-debug = False
 
 def rand_evo_count():
     global evolution_stage
@@ -88,7 +93,9 @@ def rand_type():
 
 def rand_mon_gimmick():
     global gimmick
-    special = (0, 10, 45, 25, 15, 0, 5, 5, 5)
+    global gimmick_use_method
+    global mon_has_cosmetic_forms
+    special = (0, 10, 45, 25, 15, 5, 0, 5, 5, 5)
     if "God Pokemon" in strength:
         w = special
     elif "Mythical" in strength:
@@ -96,17 +103,53 @@ def rand_mon_gimmick():
     elif "Legendary" in strength:
         w = special
     else:
-        w = (50, 20, 20, 15, 10, 15, 5, 5, 5)
+        w = (50, 20, 20, 15, 10, 5, 15, 5, 5, 5)
     mons_gimmick = random.choices(mon_gimmick, weights=(w))
     gimmick = (random.choice(mons_gimmick))
+    if "None" in gimmick:
+        pass
+    elif "Mega Evolution" in gimmick:
+        gimmick_use_method = gimmick_method[0]
+    elif "Fusion" in gimmick:
+        gimmick_use_method = gimmick_method[2]
+    elif "Gigantamax" in gimmick:
+        gimmick_use_method = gimmick_method[2]
+    elif "Signiture Move" in gimmick:
+        gimmick_use_method = gimmick_method[3]
+    elif "Ability" in gimmick:
+        while "Ability" in gimmick_use_method:
+            gimmick = random.choices(gimmick_method)
+            print(gimmick)
+    else:
+        gimmick_use_method = random.choices(gimmick_method)
+
+    form_option = [True, False]
+    mon_has_cosmetic_forms = random.choices(form_option, weights=(5,95), k=1)[0]
+
     if debug == True:
         print("Gimmick: ", gimmick)
+
+def rand_aquire_method():
+    global method_to_aquire
+    method_to_aquire = random.choices(aquire_method)
+    if "Starter" in strength:
+        method_to_aquire = special_aquire_method[5]
+    if "Special" in method_to_aquire:
+        method_to_aquire = random.choices(special_aquire_method)
+    return method_to_aquire
+
+def rand_stat_distribution():
+    global distribution_lean
+    stat_distibution_leans = ["HP", "Atk", "Def", "Sp. Atk", "Sp.Def", "Speed"]
+    distribution_lean = random.choice(stat_distibution_leans)
 
 def gen_mon():
     rand_mon_strength()
     rand_evo_count()
     rand_type()
     rand_mon_gimmick()
+    rand_aquire_method()
+    rand_stat_distribution()
 
     pokemon = {
         "Category ": strength,
@@ -119,6 +162,17 @@ def gen_mon():
         pokemon["Type 2"] = monster_type[1]
 
     pokemon["Gimmick"] = gimmick
+    if "None" in gimmick:
+        pass
+    else:
+        pokemon["Gimmick Use Method"] = gimmick_use_method
+    pokemon["Aquire Method"] = method_to_aquire
+
+    if mon_has_cosmetic_forms == True:
+        pokemon["Has Cosmetic Forms"] = mon_has_cosmetic_forms
+    
+    pokemon["Stat Distribution Lean"] = distribution_lean
+
     return pokemon
 
 for x in range(5):
